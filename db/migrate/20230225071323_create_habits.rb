fda7221565a8edfcf258c85e0a01da174451a89b
Ben Schembri <benschembri@gmail.com>
2023-02-25 18:14:27 +1100

    added habit model"

---
+++ db/migrate/20230225071323_create_habits.rb
@@ -0,0 +1,16 @@
+class CreateHabits < ActiveRecord::Migration[7.0]
+  def change
+    create_table :habits do |t|
+      t.string :name
+      t.boolean :done_today
+      t.integer :current_streak
+      t.integer :streak_record
+      t.integer :current_gap
+      t.integer :total_done
+      t.integer :total_gap
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
