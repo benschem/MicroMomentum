bfce665c765997ad8ac83d5eb5a9e13a2b4b865b
Ben Schembri <benschembri@gmail.com>
2023-03-07 16:47:46 +1100

    refactored the sentence method

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -12,36 +12,28 @@ class Habit < ApplicationRecord
     (current_streak.to_f.fdiv(longest_streak.to_f) * 100).to_i
   end
 
-  def sentence
-    done_yesterday ? (value = current_streak) : (value = current_gap)
-    sentence = []
-    case timeframe(value)
-    when 'days'
-      days = value
-      days > 1 ? sentence.push("#{days} days") : sentence.push("#{days} day")
-    when 'weeks'
-      weeks = value.divmod(7)[0]
-      weeks > 1 ? sentence.push("#{weeks} weeks") : sentence.push("#{weeks} week")
-      days = value.divmod(7)[1]
-      days > 1 ? sentence.push("#{days} days") : sentence.push("#{days} day")
-    when 'months'
-      months = value.divmod(30)[0]
-      months > 1 ? sentence.push("#{months} months") : sentence.push("#{months} month")
-      weeks = value.divmod(30)[1].divmod(7)[0]
-      weeks > 1 ? sentence.push("#{weeks} weeks") : sentence.push("#{weeks} week")
-      days = value.divmod(30)[1].divmod(7)[1]
-      days > 1 ? sentence.push("#{days} days") : sentence.push("#{days} day")
-    when 'years'
-      years = value.divmod(365)[0]
-      years > 1 ? sentence.push("#{years} years") : sentence.push("#{years} year")
-      months = value.divmod(365)[1].divmod(30)[0]
-      months > 1 ? sentence.push("#{months} months") : sentence.push("#{months} month")
-      weeks = value.divmod(365)[1].divmod(30)[1].divmod(7)[0]
-      weeks > 1 ? sentence.push("#{weeks} weeks") : sentence.push("#{weeks} week")
-      days = value.divmod(365)[1].divmod(30)[1].divmod(7)[1]
-      days > 1 ? sentence.push("#{days} days") : sentence.push("#{days} day")
-    end
-    sentence.join(", ")
+  def days_as_sentence(given_days)
+    sentence_parts = []
+
+    years, remainder = extract_timeframe(given_days, 365, 'year')
+    sentence_parts << years if years
+
+    months, remainder = extract_timeframe(remainder, 31, 'month')
+    sentence_parts << months if months
+
+    weeks, remainder = extract_timeframe(remainder, 7, 'week')
+    sentence_parts << weeks if weeks
+
+    days, remainder = extract_timeframe(remainder, 1, 'day')
+    sentence_parts << days if days
+
+    sentence_parts.compact.join(', ')
+  end
+
+  def extract_timeframe(value, timeframe_length, timeframe_name)
+    timeframe_count, remainder = value.divmod(timeframe_length)
+    timeframe_string = pluralize(timeframe_count, timeframe_name) if timeframe_count > 0
+    [timeframe_string, remainder]
   end
 
   # Need to schedule new_day to run first thing each morning
@@ -53,17 +45,22 @@ class Habit < ApplicationRecord
 
   private
 
-  def timeframe(value)
-    case value
+  def timeframe(days)
+    case days
     when 1..6
       'days'
     when 7..29
       'weeks'
     when 30..364
       'months'
-    when value > 365
+    else
       'years'
     end
+  end
+
+  def pluralize(count, singular_form)
+    plural_form = "#{singular_form}s"
+    count == 1 ? "#{count} #{singular_form}" : "#{count} #{plural_form}"
   end
 
   def mark_done
