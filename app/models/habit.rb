af593353c3f793d2a06285282dccb8148f42dae8
Ben Schembri <benschembri@gmail.com>
2023-03-07 15:20:39 +1100

    added streak days as a sentence + slight layout change

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -12,6 +12,38 @@ class Habit < ApplicationRecord
     (current_streak.to_f.fdiv(longest_streak.to_f) * 100).to_i
   end
 
+  def sentence
+    done_yesterday ? (value = current_streak) : (value = current_gap)
+    sentence = []
+    case timeframe(value)
+    when 'days'
+      days = value
+      days > 1 ? sentence.push("#{days} days") : sentence.push("#{days} day")
+    when 'weeks'
+      weeks = value.divmod(7)[0]
+      weeks > 1 ? sentence.push("#{weeks} weeks") : sentence.push("#{weeks} week")
+      days = value.divmod(7)[1]
+      days > 1 ? sentence.push("#{days} days") : sentence.push("#{days} day")
+    when 'months'
+      months = value.divmod(30)[0]
+      months > 1 ? sentence.push("#{months} months") : sentence.push("#{months} month")
+      weeks = value.divmod(30)[1].divmod(7)[0]
+      weeks > 1 ? sentence.push("#{weeks} weeks") : sentence.push("#{weeks} week")
+      days = value.divmod(30)[1].divmod(7)[1]
+      days > 1 ? sentence.push("#{days} days") : sentence.push("#{days} day")
+    when 'years'
+      years = value.divmod(365)[0]
+      years > 1 ? sentence.push("#{years} years") : sentence.push("#{years} year")
+      months = value.divmod(365)[1].divmod(30)[0]
+      months > 1 ? sentence.push("#{months} months") : sentence.push("#{months} month")
+      weeks = value.divmod(365)[1].divmod(30)[1].divmod(7)[0]
+      weeks > 1 ? sentence.push("#{weeks} weeks") : sentence.push("#{weeks} week")
+      days = value.divmod(365)[1].divmod(30)[1].divmod(7)[1]
+      days > 1 ? sentence.push("#{days} days") : sentence.push("#{days} day")
+    end
+    sentence.join(", ")
+  end
+
   # Need to schedule new_day to run first thing each morning
   def new_day
     self.done_yesterday = self.done_today
@@ -20,6 +52,19 @@ class Habit < ApplicationRecord
   end
 
   private
+
+  def timeframe(value)
+    case value
+    when 1..6
+      'days'
+    when 7..29
+      'weeks'
+    when 30..364
+      'months'
+    when value > 365
+      'years'
+    end
+  end
 
   def mark_done
     self.done_today = true
